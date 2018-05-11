e9891120ecd82a2abb6ab18835b0f98c78115ab9
Josh Snider (XBOX) <jsnider@microsoft.com>
2018-05-10 18:25:30 -0700

    fix: Check for rapidjson define before redefining.

--- source/internal/json.h
+++ source/internal/json.h
@@ -1,6 +1,10 @@
 #pragma once
 
+#ifndef RAPIDJSON_HAS_STDSTRING 
 #define RAPIDJSON_HAS_STDSTRING 1
+#elif RAPIDJSON_HAS_STDSTRING == 0
+#error "Mixer interactivity requires std::string for json parsing."
+#endif
 #include "rapidjson\document.h"
 
 namespace mixer_internal
